79928ef06ea9f146f46a85627ec63393806a300a
DreamLong <chessmrbeaston@gmail.com>
2026-07-27 11:12:03 +0200

    new worker (faster)

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,5 @@
 (async function() {
-    const API_BASE = "https://astralyxpvp.chessmrbeaston.workers.dev/api";
+    const API_BASE = "https://astralworker.chessmrbeaston.workers.dev/api";
     const IP = "java.astralyxpvp.int.yt";
 
     const escapeHtml = (s) => (s ?? '').toString().replace(/[&<>"']/g, c => ({
